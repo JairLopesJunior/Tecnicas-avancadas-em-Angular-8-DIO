76a59749144b20280bdf6817cc7726891b991fd6
unknown <jair.lopes@fatec.sp.gov.br>
2021-09-10 23:47:44 -0300

    Form Builder: Ele vai criar um formulario onde o Angular vai conseguir manipular as informações ali dentro.

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -10,20 +8,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  id: number;
   cadastro: FormGroup;
-  generos: Array<string>;
 
-  constructor(public dialog: MatDialog,
-              private fb: FormBuilder,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+  constructor(private fb: FormBuilder) { }
 
   get f() {
     return this.cadastro.controls;
   }
 
   ngOnInit(): void {
+
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required]]
+    });
   }
 
 }
